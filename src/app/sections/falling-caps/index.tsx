5cf78d35d19f3f6d3195a7957e087f3f363d1e44
aniket123de <aniketde1234@gmail.com>
2025-05-19 23:39:38 +0530

    Fixing

--- src/app/sections/falling-caps/index.tsx
+++ src/app/sections/falling-caps/index.tsx
@@ -1,10 +1,7 @@
 "use client";
 
 import * as Scrollytelling from "@bsmnt/scrollytelling";
-
 import s from "./falling-caps.module.scss";
-import { CapsModel } from "./caps";
-import { Canvas } from "@react-three/fiber";
 import { useMemo } from "react";
 
 const splitText = (text: string, wordClass?: string) => {
@@ -25,7 +22,7 @@ const splitText = (text: string, wordClass?: string) => {
   return htmlWords;
 };
 
-const lines = ["We want to help", "make the internet", "everything it can be."];
+const lines = ["We want to help", "you become a builder", "and not just a user"];
 
 export const FallingCaps = () => {
   const splittedText = useMemo(
@@ -48,18 +45,7 @@ export const FallingCaps = () => {
     <Scrollytelling.Root end="bottom bottom" debug={{ label: "Falling Caps" }}>
       <section className={s["spacer"]}>
         <div className={s["pin"]}>
-          <div className={s["canvas-container"]}>
-            <Canvas
-              camera={{ position: [0, 0, 10], fov: 35 }}
-              gl={{
-                alpha: true,
-                antialias: true,
-                powerPreference: "high-performance",
-              }}
-            >
-              <CapsModel />
-            </Canvas>
-          </div>
+          <div className={s["canvas-container"]}>{/* 3D model content removed */}</div>
 
           <p className={s["paragraph"]}>
             <Scrollytelling.Stagger
